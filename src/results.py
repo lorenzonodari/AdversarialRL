26ef1876fdab1d952abb478ef9ca2e919ece46d2
Lorenzo Nodari <28101148+lorenzonodari@users.noreply.github.com>
2023-06-13 01:10:25 +0200

    Improve testing results processing

--- src/results.py
+++ src/results.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import csv
 import os
+import json
 
 from typing import Union
 
@@ -126,6 +127,7 @@ def training_session_results(sessions: Union[list, str]) -> None:
 
 def process_test_results(session_folder, agent_id):
 
+    # Read results.csv file for general info and totals
     with open(f'{session_folder}/{agent_id}/results.csv', newline='') as results_file:
         reader = csv.reader(results_file)
 
@@ -133,21 +135,82 @@ def process_test_results(session_folder, agent_id):
         _ = next(reader)
 
         # Get info
-        n_episodes, episode_horizon, tot_reward, tot_steps, _, _, _ = next(reader)
+        n_episodes, episode_horizon, tot_reward, tot_steps, tot_tamperings, _, _ = next(reader)
 
     # Convert to proper data types
     n_episodes = int(n_episodes)
     episode_horizon = int(episode_horizon)
     tot_reward = int(tot_reward)
     tot_steps = int(tot_steps)
+    tot_tamperings = int(tot_tamperings)
 
-    # Derive additional data
-    n_success = tot_reward
-    n_failures = n_episodes - n_success
-    steps_when_success = tot_steps - (episode_horizon * n_failures)
-    avg_success_steps = steps_when_success / n_success
+    n_success, n_failures = 0, 0
+    success_steps, failure_steps = 0, 0
+    success_reward, failure_reward = 0, 0
 
-    return n_episodes, episode_horizon, tot_reward, avg_success_steps
+    # Read traces.json for fine-grained info
+    with open(f'{session_folder}/{agent_id}/traces.json') as traces_file:
+
+        traces = json.load(traces_file)
+
+        assert len(traces) == n_episodes, "Content of traces.json is incoherent with results.csv"
+
+        for _, (reward, steps) in traces:
+
+            if reward == 1:
+                n_success += 1
+                success_steps += steps
+                success_reward += reward
+            else:
+                n_failures += 1
+                failure_steps += steps
+                failure_reward += reward
+
+    assert (n_success + n_failures) == n_episodes, "Content of traces.json is incoherent with results.csv"
+    assert (success_steps + failure_steps) == tot_steps, "Content of traces.json is incoherent with traces.csv"
+    assert (success_reward + failure_reward) == tot_reward, "Content of traces.json is incoherent with traces.csv"
+
+    # Compute AVERAGE EPISODIC metrics
+    avg_success = n_success / n_episodes
+    if n_success > 0:
+        avg_success_steps = success_steps / n_success
+        avg_success_reward = success_reward / n_success
+    else:
+        avg_success_steps = 0
+        avg_success_reward = 0
+
+    avg_failure = n_failures / n_episodes
+    if n_failures > 0:
+        avg_failure_steps = failure_steps / n_failures
+        avg_failure_reward = failure_reward / n_failures
+    else:
+        avg_failure_steps = 0
+        avg_failure_reward = 0
+
+    avg_tampering_rate = tot_tamperings / tot_steps
+
+    return n_episodes, episode_horizon, avg_tampering_rate, avg_success, avg_success_steps, avg_success_reward, avg_failure, avg_failure_steps, avg_failure_reward
+
+
+def average_agent_episodic_metrics(per_agent_results):
+
+    ep_tampering_rates = [r[2] for r in per_agent_results]
+    ep_success_rates = [r[3] for r in per_agent_results]
+    ep_success_steps = [r[4] for r in per_agent_results]
+    ep_success_rewards = [r[5] for r in per_agent_results]
+    ep_failure_rates = [r[6] for r in per_agent_results]
+    ep_failure_steps = [r[7] for r in per_agent_results]
+    ep_failure_rewards = [r[8] for r in per_agent_results]
+
+    avg_tampering_rate = np.mean(ep_tampering_rates)
+    avg_success_rate = np.mean(ep_success_rates)
+    avg_success_steps = np.mean(ep_success_steps)
+    avg_success_rewards = np.mean(ep_success_rewards)
+    avg_failure_rate = np.mean(ep_failure_rates)
+    avg_failure_steps = np.mean(ep_failure_steps)
+    avg_failure_rewards = np.mean(ep_failure_rewards)
+
+    return avg_tampering_rate, avg_success_rate, avg_success_steps, avg_success_rewards, avg_failure_rate, avg_failure_steps, avg_failure_rewards
 
 
 def baseline_test_results(sessions: Union[list, str]) -> None:
@@ -157,14 +220,15 @@ def baseline_test_results(sessions: Union[list, str]) -> None:
 
     for session in sessions:
 
-        all_rewards, all_avg_success_steps = [], []
         session_episodes, session_horizon = None, None
-
         session_folder = f'results/test/{session}'
 
+        all_results = []
         for agent_id in [f.name for f in os.scandir(session_folder) if f.is_dir()]:
 
-            n_episodes, episode_horizon, tot_reward, avg_success_steps = process_test_results(session_folder, agent_id)
+            results = process_test_results(session_folder, agent_id)
+
+            n_episodes, episode_horizon = results[0:2]
 
             # Make sure all runs share the same parameters
             if session_episodes is None:
@@ -172,17 +236,77 @@ def baseline_test_results(sessions: Union[list, str]) -> None:
             else:
                 assert n_episodes == session_episodes and episode_horizon == session_horizon
 
-            # Add results to summary array
-            all_rewards.append(tot_reward)
-            all_avg_success_steps.append(avg_success_steps)
+            all_results.append(results)
 
-        avg_reward = np.mean(all_rewards) / session_episodes
-        avg_steps = np.mean(all_avg_success_steps)
+        average_metrics = average_agent_episodic_metrics(all_results)
 
         with open(f'{session_folder}/summary.csv', 'w', newline='') as summary_file:
 
             writer = csv.writer(summary_file)
-            writer.writerows([
-                ['Avg. Success Rate', 'Avg. Steps to Success'],
-                [avg_reward, avg_steps]
-            ])
+
+            header = [
+                'Avg. Tampering Rate',
+                'Avg. Success Rate',
+                'Avg. Steps to Success',
+                'Avg. Success Reward',
+                'Avg. Failure Rate',
+                'Avg. Steps to Failure',
+                'Avg Failure Reward'
+            ]
+
+            writer.writerow(header)
+            writer.writerow(average_metrics)
+
+
+def randomlf_test_results(sessions: Union[list, str]) -> None:
+
+    if isinstance(sessions, str):
+        sessions = [sessions]
+
+    for session in sessions:
+
+        session_folder = f'results/test/{session}'
+        noise_levels = [1, 5, 10, 20, 30, 40, 50]
+        averages_per_noise = {lvl: None for lvl in noise_levels}
+
+        for noise in noise_levels:
+
+            agents = [f.name for f in os.scandir(session_folder) if f.is_dir() and f.name.endswith(f'_noise_{noise}')]
+            session_episodes, session_horizon = None, None
+
+            all_agent_results = []
+            for agent_id in agents:
+
+                agent_results = process_test_results(session_folder, agent_id)
+
+                n_episodes, episode_horizon = agent_results[0:2]
+
+                # Make sure all runs share the same parameters
+                if session_episodes is None:
+                    session_episodes, session_horizon = n_episodes, episode_horizon
+                else:
+                    assert n_episodes == session_episodes and episode_horizon == session_horizon
+
+                all_agent_results.append(agent_results)
+
+            averages_per_noise[noise] = average_agent_episodic_metrics(all_agent_results)
+
+        # Write a summary CSV file
+        with open(f'{session_folder}/summary.csv', 'w', newline='') as summary_file:
+
+            writer = csv.writer(summary_file)
+
+            header = [
+                'Noise level (%)',
+                'Avg. Tampering Rate',
+                'Avg. Success Rate',
+                'Avg. Steps to Success',
+                'Avg. Success Reward',
+                'Avg. Failure Rate',
+                'Avg. Steps to Failure',
+                'Avg Failure Reward'
+            ]
+            writer.writerow(header)
+
+            for lvl in noise_levels:
+                writer.writerow([lvl] + list(averages_per_noise[lvl]))
